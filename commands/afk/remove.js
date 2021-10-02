1a3ca8b9a1c09c4788212fe0b07cf941e48de377
pavez <>
2021-10-02 02:08:13 +0000

    updated commands/afk/remove.js

--- commands/afk/remove.js
+++ commands/afk/remove.js
@@ -4,9 +4,10 @@ const RemoveAfkSpace = [
 		code: `
 $onlyIf[$dbGet[$authorID.afk.status;true;false]!=false;]
 
-$reply[$messageID;false]
-You are no longer afk
+$let[id;$sendReply[$channelID;$messageID;You are no longer afk;false;true]] $let[ch;$channelID]
+
 $dbDelete[$authorID.afk]
+$setTimeout[5s;$deleteMessage[$get[ch];$get[id]]]
 `
 	}
 ] 
