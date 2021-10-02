29d89131820ed3077a3b6a4ce03c6d45a2f15b9d
pavez <>
2021-10-02 23:18:45 +0000

    added ??wallpaper

---
+++ commands/util/wallpaper.js
@@ -0,0 +1,36 @@
+const WallpaperCmd = [
+	{
+		type: "basicCommand",
+		name: "wallpaper",
+		code: `
+$reply[$messageID;false]
+
+$title[1;Random wallpaper]
+$thumbnail[1;$authorAvatar]
+$image[1;$nekos[wallpaper]]
+$footer[1;From nekos.life]
+$color[1;001]
+$addActionRow
+$addButton[newWall;New;DANGER]
+$addButton[delRows;Select;DANGER]
+		`
+	},
+	{
+		type: "buttonCommand",
+		code: `
+$onlyIf[$interactionID==newWall]
+$updateInteraction
+
+$title[1;Random wallpaper]
+$thumbnail[1;$authorAvatar]
+$image[1;$nekos[wallpaper]]
+$footer[1;From nekos.life]
+$color[1;001]
+$addActionRow
+$addButton[newWall;New;DANGER]
+$addButton[delRows;Select;DANGER]
+		`
+	}
+]
+
+module.exports = WallpaperCmd
