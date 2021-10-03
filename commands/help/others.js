c4ff4007601da492525eb4b6daf39d2f5640a260
pavez <>
2021-10-03 01:01:30 +0000

    modified others.js

--- commands/help/others.js
+++ commands/help/others.js
@@ -15,9 +15,21 @@ $addField[1;R34;Search NSFW images from Rule34
 \`\`\`xml
 ??r34 <filter>
 \`\`\`;yes]
+$addField[1;AFK;Make the bot send a message when you are mentioned
+\`\`\`xml
+??afk [reason\\]
+\`\`\`]
 $addField[1;Remind;Reminds you of something after a certain time
 \`\`\`xml
 ??remind <time> <reminder>
+\`\`\`]
+$addField[1,New-avatar;Returns a random avatar from nekos.life
+\`\`\`xml
+??new-avatar
+\`\`\`]
+$addField[1,Wallpaper;Return a wallpaper from nekos.life
+\`\`\`xml
+??wallpaper
 \`\`\`]
 $footer[1;Developed by Kaede Studio]
 $color[1;001]
