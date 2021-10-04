8fad1be530f3c8f6a0f2003a5d9265bb6792bc30
Pavez7274 <>
2021-10-04 03:29:49 +0000

    added commands/interaction/kiss.js

---
+++ commands/interaction/kiss.js
@@ -0,0 +1,31 @@
+const KissCmd = [
+	{
+		type: "basicCommand",
+		name: "kiss",
+		code: `
+$reply[$messageID;false]
+$onlyIf[$message!=;
+$title[1;Error >> Mention a member!]
+$thumbnail[1;$authorAvatar]
+$description[1;You must mention the user with whom you want to interact]
+$color[1;001]
+]
+$onlyIf[$mentioned[1]!=$authorID;
+$title[1;You can't interact with yourself]
+$thumbnail[1;$authorAvatar]
+$color[1;001]
+]
+$onlyIf[$mentioned[1]!=;
+$callFunction[user not found]
+]
+
+$title[1;$nickname[$guildID;$authorID] kissed $nickname[$guildID;$mentioned[1]]♡♡]
+$thumbnail[1;$authorID]
+$image[1;$nekos[sfw;kiss]]
+$footer[1;From nekos.life]
+$color[1;001]
+		`
+	}
+]
+
+module.exports = KissCmd
