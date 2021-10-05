5ce7f5942a3f7ecbc63d9df6a5012a9608a19029
Pavez7274 <>
2021-10-05 23:03:35 +0000

    updated commands/interaction/pat.js

--- commands/interaction/pat.js
+++ commands/interaction/pat.js
@@ -4,29 +4,41 @@ const PatCmd = [
 		name: "pat",
 		code: `
 $reply[$messageID;false]
-$onlyIf[$message!=;
+$if[$message!=;
+$let[x;$findUser[$message]]
+;
+$let[x;nomention]
+]
+$onlyIf[$get[x]!=nomention;
 $title[1;Error >> Mention a member!]
 $thumbnail[1;$authorAvatar]
-$description[1;You must mention the user with whom you want to interact]
+$description[1;Mention the member you want to interact with]
 $color[1;001]
 ]
-$onlyIf[$mentioned[1]!=$authorID;
-$title[1;You can't interact with yourself]
+$onlyIf[$get[x]!=undefined;
+$title[1;Error >> Not found]
 $thumbnail[1;$authorAvatar]
+$description[1;The user could not be found]
 $color[1;001]
 ]
-$onlyIf[$mentioned[1]!=;
-$title[1;Error >> user not found]
+$onlyIf[$get[x]!=$authorID;
+$title[1;Error]
 $thumbnail[1;$authorAvatar]
-$description[1;The user could not be found, check that you have put everything correctly]
-$color[0;001]
+$description[1;You can't interact with yourself!]
+$color[1;001]
+]
+$onlyIf[$memberExists[$guildID;$get[x]]!=false;
+$title[1;Error]
+$thumbnail[1;$authorAvatar]
+$description[1;The user [<@!$get[x]>\\] is not part of the server]
+$color[1;001]
 ]
 
-$title[1;$nickname[$guildID;$authorID] patting $nickname[$guildID;$mentioned[1]]]
-$thumbnail[1;$authorAvatar]
+$title[1;$nickname[$guildID;$authorID] pats $nickname[$guildID;$get[x]]]
 $image[1;$nekos[sfw;pat]]
 $footer[1;From nekos.life]
 $color[1;001]
+$callFunction[log]
 		`
 	}
 ]
