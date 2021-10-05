d78fb7e73a94336e2e7e42aec5b5eb2727ff3a5c
Pavez7274 <>
2021-10-05 23:02:25 +0000

    updated commands/interaction/hug.js

--- commands/interaction/hug.js
+++ commands/interaction/hug.js
@@ -7,7 +7,13 @@ $reply[$messageID;false]
 $if[$message!=;
 $let[x;$findUser[$message]]
 ;
-$let[x;undefined]
+$let[x;nomention]
+]
+$onlyIf[$get[x]!=nomention;
+$title[1;Error >> Mention a member!]
+$thumbnail[1;$authorAvatar]
+$description[1;Mention the member you want to interact with]
+$color[1;001]
 ]
 $onlyIf[$get[x]!=undefined;
 $title[1;Error >> Not found]
@@ -24,6 +30,7 @@ $color[1;001]
 
 $title[1;$nickname[$guildID;$authorID] hugged $nickname[$guildID;$get[x]]]
 $image[1;$nekos[sfw;hug]]
+$footer[From nekos.life]
 $color[1;001]
 $callFunction[log]
 		`
@@ -36,7 +43,13 @@ $reply[$messageID;false]
 $if[$message!=;
 $let[x;$findUser[$message]]
 ;
-$let[x;undefined]
+$let[x;nomention]
+]
+$onlyIf[$get[x]!=nomention;
+$title[1;Error >> Mention a member!]
+$thumbnail[1;$authorAvatar]
+$description[1;Mention the member you want to interact with]
+$color[1;001]
 ]
 $onlyIf[$get[x]!=undefined;
 $title[1;Error >> Not found]
@@ -50,9 +63,16 @@ $thumbnail[1;$authorAvatar]
 $description[1;You can't interact with yourself!]
 $color[1;001]
 ]
+$onlyIf[$memberExists[$guildID;$get[x]]!=false;
+$title[1;Error]
+$thumbnail[1;$authorAvatar]
+$description[1;The user [<@!$get[x]>\\] is not part of the server]
+$color[1;001]
+]
 
 $title[1;$nickname[$guildID;$authorID] hugged $nickname[$guildID;$get[x]]]
 $image[1;$nekos[sfw;cuddle]]
+$footer[1;From nekos.life]
 $color[1;001]
 $callFunction[log]
 		`
