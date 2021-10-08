45b7b8bd38a413351281160456a93a796a3b2ba2
Pavez7274 <>
2021-10-08 01:31:44 +0000

    updated commands/util/stats.js

--- commands/util/stats.js
+++ commands/util/stats.js
@@ -10,10 +10,8 @@ $thumbnail[1;$authorAvatar]
 $addField[1;Ping;$ping;true]
 $addField[1;Cpu;$toFixed[$cpu;2]%;true]
 $addField[1;Ram;$toFixed[$ram;2]MB;true]
-$addField[1;DataBase;Size: $toFixed[$math[$fileSize[json.sqlite]/1024];2]MBs
-Type: sqlite
-Package: quick.db
-Version: 7.1.3;true]
+$addField[1;DataBase;Package: @replit/datbase
+Version: 2.0.1;true]
 $addField[1;UpTime;<t:$toFixed[$math[($dateNow/1000)-($uptime/1000)];0]:R>;true]
 $addField[1;API Status;$httpGet[https://lappland.kaedestudio.ga/;status;];true]
 $color[1;001]
