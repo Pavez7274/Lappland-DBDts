3652b65162f9fb9bcf7cacbd661a9a7695a1f965
Pavez7274 <>
2021-10-08 01:26:18 +0000

    updated commands/dev tools/reboot.js - Now it shows information in the console about the reboot

--- commands/dev tools/reboot.js
+++ commands/dev tools/reboot.js
@@ -9,6 +9,13 @@ $callFunction[devs]
 $let[ch;$channelID]
 $let[msj;$sendReply[$get[ch];$messageID;Rebooting;false;true]]
 
+$if[$message!=;$let[reason;$message];$let[reason;No reason was given]]
+$console[|--------------[DEBUG\\]--------------|
+| Action: Reboot
+| Executed by: $userTag - $authorID
+| Reason: > $get[reason] <
+| Stamp: $dateNow
+|-----------------------------------|;debug]
 $setTimeout[2s;$deleteMessage[$get[ch];$get[msj]]
 $reboot]
 `
