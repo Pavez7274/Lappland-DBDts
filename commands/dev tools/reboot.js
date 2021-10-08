ef272bbd16ddb282a48cf523034e29890e6eea6d
Pavez7274 <>
2021-10-08 05:55:29 +0000

    New reboot system by yuka!!

--- commands/dev tools/reboot.js
+++ commands/dev tools/reboot.js
@@ -4,21 +4,70 @@ const RebootCmd = [
 		name: "reboot", 
 		aliases: ["rt"], 
 		code: `
+$reply[$messageID;false]
 $callFunction[devs]
 
-$let[ch;$channelID]
-$let[msj;$sendReply[$get[ch];$messageID;Rebooting;false;true]]
-
-$if[$message!=;$let[reason;$message];$let[reason;No reason was given]]
-$console[|--------------[DEBUG\\]--------------|
-| Action: Reboot
-| Executed by: $userTag - $authorID
-| Reason: > $get[reason] <
-| Stamp: $dateNow
-|-----------------------------------|;debug]
-$setTimeout[2s;$deleteMessage[$get[ch];$get[msj]]
-$reboot]
+$title[1;Reboot options]
+$thumbnail[1;$authorAvatar]
+$addField[1;All files;Restart all routes, whether they are part of the bot or not]
+$addField[1;Cdm only;Only the commands are restarted, both those that are in the main file and those that are loaded by handler]
+$color[1;001]
+$addActionRow
+$addButton[reboot;All files;DANGER]
+$addButton[updates;Cdm only;DANGER]
+$addButton[delete;Delete this;DANGER]
 `
+	},
+	{
+		type: "buttonCommand",
+		code: `
+$onlyIf[$interactionID==reboot;]
+$onlyForIDs[$pavez[id];$yuka[id];$guillermo[id];660970791202062381;
+$ephemeral
+$title[1;Error >> Required permissions]
+$thumbnail[1;$authorAvatar]
+$description[1;This command is for developers only]
+$color[1;001]
+]
+$deleteMessage[$channelID;$messageID]
+
+$ephemeral
+The files are being restarted, remember that each restart is saved in the database
+$setTimeout[2s;$reboot]
+		`
+	},
+	{
+		type: "buttonCommand",
+		code: `
+$onlyIf[$interactionID==update;]
+$onlyForIDs[$pavez[id];$yuka[id];$guillermo[id];660970791202062381;
+$ephemeral
+$title[1;Error >> Required permissions]
+$thumbnail[1;$authorAvatar]
+$description[1;This command is for developers only]
+$color[1;001]
+]
+$deleteMessage[$channelID;$messageID]
+
+$ephemeral
+Commands are being restarted, remember that each restart is saved in database
+$setTimeout[2s;$updateCommands]
+		`
+	},
+	{
+		type: "buttonCommand",
+		code: `
+$onlyIf[$interactionID==delete;]
+$onlyForIDs[$pavez[id];$yuka[id];$guillermo[id];660970791202062381;
+$ephemeral
+$title[1;Error >> Required permissions]
+$thumbnail[1;$authorAvatar]
+$description[1;This command is for developers only]
+$color[1;001]
+]
+
+$deleteMessage[$channelID;$messageID]
+		`
 	}
 ]  
 
