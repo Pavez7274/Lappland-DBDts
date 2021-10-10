01260c0222df157de2696cebb86eba42fd863e24
Pavez7274 <>
2021-10-10 23:59:46 +0000

    corregido el "error" que impedia subir de nivel

--- commands/rank/up.js
+++ commands/rank/up.js
@@ -10,10 +10,16 @@ $let[cd;$sub[$dateNow;10]]
 ]
 $onlyIf[$get[cd]<=$dateNow;]
 
+$if[$db[get;xp_$authorID;0]>=$db[get;exp_$authorID;500];
+$db[delete;xp_$authorID]
+$db[set;exp_$authorID;$sum[$db[get;exp_$authorID;500];500]]
+$db[set;level_$authorID;$sum[$db[get;level_$authorID;0];1]]
+;
 $let[new;$sum[$db[get;xp_$authorID;0];$random[14]]]
 $let[newstamp;$sum[$dateNow;5000]]
 $db[set;xp_$authorID;$get[new]]
 $db[set;cooldown_$authorID_levelup;$get[newstamp]]
+]
 		`
 	}
 ]
