b4897e7ae0ce91b9f2ba26652498f2d08260baf2
Pavez7274 <>
2021-10-15 17:54:49 +0000

    updated server.js

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
+// Requirements
 const express = require('express');
 const server = express()
+const path = require("path")
+const database = require('@replit/database')
+const db = new database()
 
 // Configs
 server.set('json spaces', 2)
+server.engine("html", require("ejs").renderFile)
+server.set("view engine", "ejs")
+server.set("views", path.join(__dirname, "src/views/"))
 
 // Rutas
 server.get("/", (req, res) => {
 	res.json({status:res.statusCode})
+})
+server.get("/app",(req, res) => {
+	res.render("app.html", {
+		title: "App",
+		db: db
+	})
 })
 
 // Listen 
