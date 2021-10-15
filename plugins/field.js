53fbd337a8f8cf5c110f1cd3c19149f67fe91e9f
Pavez7274 <>
2021-10-15 17:59:45 +0000

    added [index]

---
+++ plugins/field.js
@@ -0,0 +1,24 @@
+/**
+ * By Pavez#7274
+ * @type { import("dbd.ts").FunctionData }
+ */ 
+const func = {
+	name: "$field",
+	description: "no desc",
+	brackets: true,
+  execute: async (d, fn) => {
+		const [field] = fn.resolveArray(d)
+    return fn.resolve(
+			d.data.args.join(" ").split(" ?? ")[field]
+    )
+  },
+	fields: [
+		{
+			name: "field",
+			type: "NUMBER",
+			required: true
+		}
+	]
+}
+
+module.exports = func
