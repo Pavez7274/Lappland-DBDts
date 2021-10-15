4425c02c9c0e4b70db5d582df50d8c9891108e2b
Pavez7274 <>
2021-10-15 12:59:32 +0000

    added [key;value;ttl (optional);eval (optional)] and [key;eval (optional)]

---
+++ plugins/cacheGet.js
@@ -0,0 +1,47 @@
+/**
+ * By Pavez#7274 
+ * @type { import("dbd.ts").FunctionData }
+ */
+const func = {
+	name: "$cacheSet",
+	description: "cache manager",
+	brackets: true,
+	execute: async (d, fn) => {
+		const DBDJSDB = require("dbdjs.db")
+		const db = new DBDJSDB.Database({
+			path: "./database/",
+			tables: [{ name: "main" }, { name: "dev" }, { name: "cache" }]
+		})
+		db.connect()
+		let [key, Eval=``] = await fn.resolveArray(d)
+		// Database connection
+
+		try{
+			let result = db.get("cache", key).then((data) => eval(Eval))
+		} catch (error) {
+			console.log(error.message)
+			d.sendError(fn, `:x: Cache error: ${error.message}`)
+			return fn.resolve()
+		}
+
+		if (typeof r == "object") result = require("util").inspect(result, { depth: max })
+
+		return fn.resolve(
+			result
+		)
+	},
+	fields: [
+		{
+			name: "key",
+			required: true,
+			type: "STRING"
+		},
+		{
+			name: "eval",
+			required: false,
+			type: "STRING"
+		}
+	]
+}
+
+module.exports = func
---
+++ plugins/cacheSet.js
@@ -0,0 +1,51 @@
+/**
+ * By Pavez#7274 
+ * @type { import("dbd.ts").FunctionData }
+ */
+const func = {
+	name: "$cacheSet",
+	description: "cache manager",
+	brackets: true,
+	execute: async (d, fn) => {
+		const DBDJSDB = require("dbdjs.db")
+		const db = new DBDJSDB.Database({
+			path: "./database/",
+			tables: [{ name: "main" }, { name: "dev" }, { name: "cache" }]
+		})
+		db.connect()
+		let [key, value, ttl=60000, Eval=`console.log(\`|---------------[cache]---------------|
+| Name: \${key}
+| Value: \${value}
+| TTL: \${ttl}
+|-------------------------------------|\`)`] = await fn.resolveArray(d)
+		// Database connection
+
+		db.set("cache", key, value, ttl).then((data) => eval(Eval))
+
+		return fn.resolve()
+	},
+	fields: [
+		{
+			name: "key",
+			required: true,
+			type: "STRING"
+		},
+		{
+			name: "value",
+			required: false,
+			type: ["STRING", "BOOLEAN", "NUMBER", "TIME"]
+		},
+		{
+			name: "ttl",
+			required: false,
+			type: "NUMBER"
+		},
+		{
+			name: "eval",
+			required: false,
+			type: "STRING"
+		}
+	]
+}
+
+module.exports = func
