a07fb20907e9bd616e85b3dbc1f61f60785a3c27
Pavez7274 <>
2021-10-19 17:44:36 +0000

    fixed ??r34 (command)

--- commands/util/r34.js
+++ commands/util/r34.js
@@ -24,15 +24,13 @@ $thumbnail[1;$authorAvatar]
 $description[1;This channel is not NSFW!]
 $color[1;001]
 ]
-$let[l;$r34[$message;count]]
-$let[x;$r34[$message;url;$random[$get[l]]]]
-$onlyIf[$get[x]!=;
+$onlyIf[$r34[$message]!=;
 $title[1;Error >> Not found!]
 $thumbnail[1;$authorAvatar]
 $color[1;001]
 ]
 
-$image[1;$get[x]]
+$image[1;$r34[$message]]
 $color[1;001]
 
 $callFunction[log]
