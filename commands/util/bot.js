d5253b05c5c2dfa54b968d4706f0d20cefcc205a
Pavez7274 <>
2021-10-21 22:54:48 +0000

    updated ??bot

--- commands/util/bot.js
+++ commands/util/bot.js
@@ -21,8 +21,8 @@ $thumbnail[1;$authorAvatar]
 $addField[1;Ping;$ping;true]
 $addField[1;Cpu;$toFixed[$cpu;2]%;true]
 $addField[1;Ram;$toFixed[$ram;2]MB;true]
-$addField[1;DataBase;Package: @replit/database
-Version: 2.0.1;true]
+$addField[1;DataBase;Package: dbdjs.db
+Version: 2.0.2;true]
 $addField[1;UpTime;<t:$toFixed[$math[($dateNow/1000)-($uptime/1000)];0]:R>;true]
 $addField[1;API Status;$httpGet[https://lappland.kaedestudio.ga/;status;];true]
 $color[1;001]
