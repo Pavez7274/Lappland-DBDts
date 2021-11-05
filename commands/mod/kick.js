11ffc1f0aaa63e8625f19caa16712815215ef705
Pavez7274 <>
2021-11-05 23:26:31 +0000

    added error suppression in `kick`

--- commands/mod/kick.js
+++ commands/mod/kick.js
@@ -7,11 +7,12 @@ $onlyIf[$endsWith[$message;--help]!=true;
 $title[1;Help >> Kick]
 $thumbnail[1;$authorAvatar]
 $description[1;**usage**
-\`\`\`
-??kick <user>
+\`\`\`xml
+??kick <user> [reason\\]
 \`\`\`
 **fields**
-user: snowflake, mention, usertag, nickname]
+user: snowflake, mention, usertag, nickname
+reason: string]
 $color[1;001]
 ]
 $if[$authorID!=$ownerID;$onlyIf[$hasPerm[$guildID;$authorID;kickmembers]==true;
@@ -20,7 +21,9 @@ $thumbnail[1;$authorAvatar]
 $description[1;To use this command you need permission to kick members from the server]
 $color[1;001]
 ]]
-$let[id;$findMember[$guildID;$message]]
+$let[id;$findMember[$guildID;$message[0]]]
+$if[$message[2]!=;$let[r;$messageSlice[1]];$let[r;No reason was given]]
+
 $onlyIf[$get[id]!=;
 $title[1;Error >> Mention a member!]
 $thumbnail[1;$authorAvatar]
@@ -40,7 +43,16 @@ $description[1;I can't kick the server owner]
 $color[1;001]
 ]
 
-$ban[$guildID;$get[id];$messageSlice[1]]
+$suppressErrors[
+$title[1;Error]
+$thumbnail[1;$authorAvatar]
+$description[1;There was an error at the time of doing this action, please review the following
+- That I am in a hierarchical position higher than the user
+- That I have permission to kick users]
+$color[1;001]
+]
+
+$ban[$guildID;$get[id];$get[r]]
 $title[1;Kicked Member]
 $thumbnail[1;$userAvatar[$get[id]]]
 $description[1;$userTag[$get[id]] was kicked from the server by $userTag[$authorID]]
