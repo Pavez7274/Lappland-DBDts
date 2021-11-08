641792adc9d48210f58bb13c16071407f47079e4
Pavez7274 <>
2021-11-08 22:06:57 +0000

    fixed  event

--- commands/snipe/event.js
+++ commands/snipe/event.js
@@ -37,15 +37,15 @@ $js[false;
 			content: 'undefined'
 		}
 	\\]
-	db.get('main', \`snipe_\${d.data.message?.channel?.id}\`).then(data => {
+	db.get('main', \`snipe_\${d.data.extras.oldMessage?.channel?.id}\`).then(data => {
 		const arr = data?.value || def
 
 		arr.push({
 			type: 'edited', 
-			author: d.data.message?.author?.id || 'undefined', 
-			content: d.data.message?.content || 'undefined'
+			author: d.data.extras.oldMessage?.author?.id || 'undefined', 
+			content: d.data.extras.oldMessage?.content || 'undefined'
 		})
-		db.set('main', \`snipe_\${d.data.message?.channel?.id}\`, arr)
+		db.set('main', \`snipe_\${d.data.extras.oldMessage?.channel?.id}\`, arr)
 	})
 
 ;10]
