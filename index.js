af40ebe01fc72e439b659c29eb40f53ffa069d2d
Pavez7274 <>
2021-11-13 03:55:04 +0000

    updated index.js

--- index.js
+++ index.js
@@ -1,35 +1,10 @@
-const dbd = require("dbd.ts")
+const { Bot } = require("dbd.ts")
 const colors = require('colors')
-const client = new dbd.Bot({
-  intents: [
-		'GUILDS',
-		'GUILD_MESSAGES', 
-		'GUILD_MEMBERS',
-		'DIRECT_MESSAGES',
-		'GUILD_MESSAGE_REACTIONS'
-	],
-  prefix: {
-    mentionPrefix: true,
-    prefixes: ['lappland ', '??']
-  },
-	database: {
-		path: `./database/default.sqlite`
-	},
-	internalSharding: true
-})
+const { BotOptions } = require('./handlers/options.js')
+const client = new Bot(BotOptions)
 
 // Database
 const db = require('./db.js')
-
-// Ready
-client.commands.add({
-	type: "readyCommand",
-	code: `
-$js[false;
-console.log('DBD.TS'.red, '- Ready on client', String(d.client.user.tag).blue)
-;2]
-	`
-})
 
 // StatusManager
 require(`./handlers/status.js`)(client)
@@ -53,31 +28,5 @@ require(`./handlers/slash.js`)(client)
 client.login(process.env['token'])
 
 
-/*          DISCORD.JS CODE           */
-
-const djs = require('./discordjs/connection.js')
-
-
 /*               24/7               */
-const keepAlive = require('./server')
-const Monitor = require('ping-monitor')
-
-keepAlive()
-const monitor = new Monitor({
-	website: 'https://lappland.kaedestudio.ga',
-	title: 'Lappland',
-	interval: 2
-})
-
-monitor.on('up', async (res) => {
-	console.log('MONITOR'.brightGreen, '-', 'uptime started.')
-})
-monitor.on('down', (res) => {
-	console.log('MONITOR'.brightGreen, '-', 'uptime has down.')
-})
-monitor.on('stop', (website) => {
-	console.log('MONITOR'.brightGreen, '-', 'uptime has stopped.')
-})
-monitor.on('error', (error) => {
-	console.log('MONITOR'.brightGreen, '-', 'ERROR', '-', `${error}`.red)
-})
+require('./server')()
