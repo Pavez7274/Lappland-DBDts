b175dfe43043699e6fde57c3c44007585ca56047
Pavez7274 <>
2021-11-16 23:25:23 +0000

    updated server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const server = express()
 const path = require('path')
 const db = require('./db.js')
 const djs = require('./discordjs/connection.js')
+const colors = require('colors')
 
 // Configs
 server.set('json spaces', 2)
@@ -11,11 +12,17 @@ server.engine('html', require('ejs').renderFile)
 server.set('view engine', 'ejs')
 server.set('views', path.join(__dirname, 'src/views/'))
 
-// Rutas
+// Routes
 server.get('/', (req, res) => {
 	res.json({
 			status:res.statusCode,
-			web: 'https://lappland.kaedestudio.ga/app'
+			web: 'https://lappland.kaedestudio.ga/app',
+			lavalink: {
+				url: 'https://lava.pavez.ga',
+				password: 'youshallnotpass',
+				port: 443,
+				secure: true
+			}
 		})
 })
 server.get('/app', async (req, res) => {
@@ -24,19 +31,11 @@ server.get('/app', async (req, res) => {
 		allBots: await db.all('main', { filter: ({ data }) => data.key.includes('botlist_bot') }).then(bs => bs.sort((x, y) => y.data.value.votes - x.data.value.votes))
 	})
 })
-server.get('/nekos', async (req, res) => {
-	const Neko = require('nekos.life');
-	const nekos = new Neko()
-
-	res.json({
-		url: await nekos.nsfw.neko()
-	})
-})
 
 // Listen 
 module.exports = () => {
-    server.listen(3000, () => {
-        console.log('Servidor listo!');
-    })
-    return true;
+  server.listen(3000, () => {
+    console.log('EXPRESS'.red, '- server ready');
+  })
+  return true;
 }
