714923ad0e91e1a06816f1d25b0f560801554a92
Pavez7274 <>
2021-11-20 02:19:32 +0000

    `??jumbo` now accepts ids and names

--- commands/util/jumbo.js
+++ commands/util/jumbo.js
@@ -5,10 +5,31 @@ const JumboCmd = [
 		aliases: [ 'emote', 'emoji' ],
 		code: `
 $reply[$messageID;false]
+$onlyIf[$endsWith[$message;--help]!=true;
+$title[1;Help >> Jumbo]
+$thumbnail[1;$authorAvatar]
+$description[1;**usage**
+\`\`\`
+??jumbo <emote>
+\`\`\`
+**fields**
+emote: string, number
+
+**returns**
+Image]
+$color[1;001]
+]
+
+$onlyIf[$message!=;
+$title[1;Error >> Field >> Empty]
+$thumbnail[1;$authorAvatar]
+$description[1;\`Field 1\` [emote\\] is empty]
+$color[1;001]
+]
 
 $js[false;
 
-	const emote = d.client.emojis.cache.find(e => e.name.toLowerCase() === d.data.args[0\\].split(':')[1\\]?.toLowerCase())
+	const emote = d.client.emojis.cache.find(e => e.name.toLowerCase() === d.data.args[0\\].split(':')[1\\]?.toLowerCase() || e.id === d.data.args[0\\] || e.name.toLowerCase() === d.data.args[0\\].toLowerCase())
 	d.keywords = {
 		returnError: !emote,
 		url: emote?.url,
